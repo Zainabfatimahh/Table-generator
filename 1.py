3317fa6f9e844ed45eb992252e1a65361fd36398
Zainabfatimahh <xainabfatimahh@gmail.com>
2026-01-12 21:53:01 -0800

    Add multiplication table output example to 1.py

--- 1.py
+++ 1.py
@@ -4,3 +4,19 @@ print(f"\nMultiplication Table of {num}:\n")
 
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
+
+## Example Output:
+'''Enter a number: 5
+
+Multiplication Table of 5:
+
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50'''
